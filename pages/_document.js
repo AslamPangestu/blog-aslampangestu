07a2f68825562d3c079b86787f54f7b98cdb14f0
aslampangestu <aslampangestu@gmail.com>
2024-02-16 12:00:51 +0700

    fix eslint

--- pages/_document.js
+++ pages/_document.js
@@ -15,7 +15,10 @@ class MyDocument extends Document {
             rel="stylesheet"
             href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
           />
-          <meta name="google-site-verification" content="2GGIUGf6xaRD7VO2QMtamyXArMPJGnVqyLUgTnBt8XQ" />
+          <meta
+            name="google-site-verification"
+            content="2GGIUGf6xaRD7VO2QMtamyXArMPJGnVqyLUgTnBt8XQ"
+          />
         </Head>
         <body>
           <Main />
